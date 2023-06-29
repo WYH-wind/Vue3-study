c76d4a982a4826145582663fa5e32da3f4547002
wangyihuang <wangyihuang@lixiang.com>
2023-06-29 17:51:49 +0800

    feat: 提交保存

---
+++ vue.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+  <!-- vue3支持多个 -->
+  <!-- <div></div> -->
+  <!-- <div></div> -->
+  <!-- <div></div> -->
+</template>
+
+<script setup lang="ts">
+// import {watch} from 'vue'
+export default {
+  setup() {
+    
+  },
+}
+</script>
+
+<style>
+*{
+  padding: 0;
+  margin: 0 ;
+}
+</style>
